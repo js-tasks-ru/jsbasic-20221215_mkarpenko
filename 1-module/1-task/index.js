c7cb1ef11b900d831039f1fca0639f36dc7e2b76
Maxim Karpenko <mkarpenko@appulate.com>
2022-12-20 19:41:52 +0300

    add [factorial] function

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1,
+      counter = 1;
+
+  if (n == 0 || n == 1) {
+    return result;
+  }
+  
+  while(counter <= n) {
+    result *= counter;
+    counter++;
+  }
+  
+  return result;
 }
