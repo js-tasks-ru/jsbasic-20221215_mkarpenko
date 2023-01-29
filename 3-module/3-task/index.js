95dba7ce594da89fa3a560d7228333ead02a0f5f
Maxim Karpenko <mkarpenko@appulate.com>
2023-01-30 01:32:06 +0300

    add [camelize] function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  const words = str.split('-');
+  const result = [ words[0] ];
+  
+  words.map(function(word, index) {
+    if(index) {
+      word = word.replace(word[0], word[0].toUpperCase());
+      result.push(word);
+    }
+  })
+
+  return result.join('');
 }
