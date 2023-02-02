e43dfdbfea7d0285b251a28bad6532d57af3d3fb
Maxim Karpenko <mkarpenko@appulate.com>
2023-02-02 23:27:03 +0300

    add [makeFriendsList] function

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('UL');
+  for (let object of friends) {
+    const li = document.createElement('LI');
+    li.textContent = `${object.firstName} ${object.lastName}`;
+    ul.appendChild(li);
+  }
+  return ul;
 }
