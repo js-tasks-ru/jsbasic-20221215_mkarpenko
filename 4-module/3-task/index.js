a63e50dd8f5202b38abb00dd25f44783b1fdabeb
Maxim Karpenko <mkarpenko@appulate.com>
2023-02-07 17:58:28 +0300

    add [highlight] function

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.tBodies[0].rows;
+  
+  for(let row of rows) {
+
+    let isAvailable = row.cells[3].dataset.available === 'true';
+    let hasDataAttr = row.cells[3].hasAttribute('data-available');
+    let isMale =      row.cells[2].textContent === 'm';
+    let age =         row.cells[1].textContent;
+    
+    if (hasDataAttr) {
+      row.classList.add( isAvailable ? 'available' : 'unavailable' );
+    }
+    if (!hasDataAttr) {
+      row.hidden = true;
+    }
+    if (true) {
+      row.classList.add( isMale ? 'male' : 'female' );
+    }
+    if (age < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
